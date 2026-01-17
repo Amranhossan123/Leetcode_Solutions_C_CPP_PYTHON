77e7ac976b93773d317d1d2198392053aead9c29
Amranhossan123 <mdimranh080@gmail.com>
2026-01-17 09:25:15 +0600

    Counting

---
+++ Counting/169_Majority Element.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+        
+        return candidate
+    
+if __name__=="__main__":
+    obj=Solution()
+    nums = [2,2,1,1,1,2,2]
+    result=obj.majorityElement(nums)
+    print(result)
