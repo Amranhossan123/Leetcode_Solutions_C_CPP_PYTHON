9aa90aedaaa08e903a15e29199464180ab7d2789
Amranhossan123 <mdimranh080@gmail.com>
2026-02-22 23:51:05 +0600

    Two_Pointers

---
+++ Two_Pointers/925_Long Pressed Name.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        i=0
+        j=0
+        while j<len(typed):
+            if i<len(name) and name[i]==typed[j]:
+                i+=1
+                j+=1
+            elif j>0 and typed[j]==typed[j-1]:
+                j+=1
+            else:
+                return False
+        return i==len(name)
+        
+
+if __name__=="__main__":
+    obj=Solution()
+    name = "saeed"
+    typed = "ssaaedd"
+    result=obj.isLongPressedName(name,typed)
+    print(result)
